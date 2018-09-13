fb7e7632829d4dcbde90c212026a15b2abdb1e16
MormonJesus69420 <egeikkjenorsk@gmail.com>
2018-09-13 19:20:03 +0200

    Done with first part of exercise.

--- Assignment1/Car.py
+++ Assignment1/Car.py
@@ -1,28 +1,147 @@
 from dataclasses import dataclass, field
 from enum import IntEnum
 from random import choice
+from numpy import array, zeros
 
 
 class Action(IntEnum):
+    """An enum class used to represent actions for cars.
+
+    Attributes:
+        WAIT (int): Wait action variable (is 0).
+        DRIVE (int): Drive action variable (is 1).
+    """
+
     WAIT = 0
     DRIVE = 1
 
     def __str__(self) -> str:
+        """Method for getting name of action in readable format.
+
+        Returns:
+            str: Name of action in nice format.
+        """
+
         return self.name.title()
+
+
+def make_new_q_matrix() -> array:
+    """Method used to initialize Q matrix for learning car.
+
+    Returns:
+        array: A 2x2 NumPy array with zeroes.
+    """
+
+    return zeros((2, 2))
+
+
+def random_action() -> Action:
+    """Method for getting a random action.
+
+    Returns:
+        Action: A random action value.
+    """
+
+    return choice([Action.WAIT, Action.DRIVE])
 
 
 @dataclass
 class Car:
+    """A super class for representing a car in simulation.
+
+    Attributes:
+        action (Action): Action currently chosen by car (default is WAIT).
+        drove_over (bool): Whether car has driven over bridge (default is False).
+        score (int): Score earned by car in simulation.
+    """
+
     action: Action = field(default=Action.WAIT, repr=False, init=False)
     drove_over: bool = field(default=False, repr=False, init=False)
     score: int = 0
 
-    def make_choice(self):
+    def take_action(self) -> None:
+        """Takes action for car, NOT IMPLEMENTED."""
+
+        pass
+
+    def reward_action(self, reward: int) -> None:
+        """Rewards action taken by car, NOT IMPLEMENTED.
+
+        Arguments:
+            reward (int): Reward value, can be negative.
+        """
+
         pass
 
 
 @dataclass
 class RandomCar(Car):
-    def make_choice(self):
-        self.action = choice([Action.WAIT, Action.DRIVE])
+    """Zero intelligence car, takes random actions and learns nothing."""
 
+    def take_action(self) -> None:
+        """Takes random action for car."""
+
+        self.action = random_action()
+
+    def reward_action(self, reward: int) -> None:
+        """Rewards action taken by car, simply updates score.
+
+        Arguments:
+            reward (int): Reward value, can be negative.
+        """
+
+        self.score += reward
+
+        self.action = Action.WAIT if self.drove_over else self.action  # Done here so that simulate class is versatile.
+
+
+@dataclass
+class LearningCar(Car):
+    """Learning car, takes actions based on Q-Learning algorithm.
+
+    Attributes:
+        q_matrix (array): Q matrix for car (default is 2x2 zero matrix).
+        state (Action): Current state for car (default is either WAIT or DRIVE).
+        learn_factor (float): Learning factor for car (default is 0.9).
+        decay_factor (float): Decay factor for car (default is 0.5).
+    """
+
+    q_matrix: array = field(default_factory=make_new_q_matrix, repr=False, init=False)
+    state: Action = field(default_factory=random_action, repr=False, init=False)
+    learn_factor: float = 0.9
+    decay_factor: float = 0.5
+
+    def take_action(self) -> None:
+        """Takes action for car based on its current state and Q matrix.
+
+        If all actions have equal Q value, take random action. Take action with
+        biggest Q value otherwise.
+        """
+
+        col = self.q_matrix[:, self.state]  # Get column for current state
+        if col[Action.WAIT] == col[Action.DRIVE]:  # Check if equal in column
+            self.action = random_action()  # Random choice
+        else:
+            self.action = Action(col.argmax())  # Choose best value from column
+
+    def reward_action(self, reward: int) -> None:
+        """Rewards action taken by car using Q-Learning algorithm.
+
+        Using reward argument and Q-Learning algorithm updates learns the car to
+        take actions. Updates score and state for car.
+
+        Arguments:
+            reward (int): Reward value, can be negative.
+        """
+
+        a = self.action
+        s = self.state
+        q = self.q_matrix[s, a]  # Q value for state change
+        q_next = self.q_matrix[a, self.q_matrix[a].argmax()]  # Q value for best action in new state
+
+        # Update value in Q matrix
+        self.q_matrix[s, a] += self.learn_factor * (reward + self.decay_factor * q_next - q)
+
+        self.state = self.action  # Action is the new state
+        self.score += reward  # Update score
+        self.action = Action.WAIT if self.drove_over else self.action  # Done here so that simulate class is versatile.
--- Assignment1/Simulation.py
+++ Assignment1/Simulation.py
@@ -3,110 +3,159 @@ from typing import List
 from enum import IntEnum
 from matplotlib import pyplot
 
-from Car import Action, Car, RandomCar
+from Car import Action, Car, RandomCar, LearningCar
 
 
 class State(IntEnum):
+    """An enum class used to represent states in simulation.
+
+    Attributes:
+        CRASH (int): Crash state variable (is 0).
+        BOTH_WAIT (int): Both cars wait state variable (is 1).
+        CAR_1_DROVE (int): Car 1 drove while 2 waited state variable (is 2).
+        CAR_2_DROVE (int): Car 2 drove while 1 waited state variable (is 3).
+    """
+
     CRASH = 0
     BOTH_WAIT = 1
     CAR_1_DROVE = 2
     CAR_2_DROVE = 3
 
     def __str__(self) -> str:
+        """Method for getting name of state in readable format.
+
+        Returns:
+            str: Name of state in nice format.
+        """
+
         return self.name.lower().replace("_", " ")
 
 
 @dataclass
 class Simulation:
+    """A class for simulating one lane bridge crossing problem.
+
+    It takes in two cars and uses them to simulate games finally showing a graph
+    with scores for each cars gained through simulation. Simulates games where
+    each car tries to cross a one lane bridge. Each game lasts until both cars
+    have safely crossed the bridge or crashed.
+
+    Attributes:
+        car1 (Car): First car to be used in simulation.
+        car2 (Car): Second car to be used in simulation.
+        car1_scores (List[int]): List of scores for car 1 after each game
+        (default is an empty list).
+        car2_scores (List[int]): List of scores for car 2 after each game
+        (default is an empty list).
+        state (State): State for current turn in game (default is BOTH_WAIT).
+        debug (bool): Whether to print status messages to (default is False).
+    """
+
     car1: Car
     car2: Car
-    state: State = State.BOTH_WAIT
     car1_scores: List[int] = field(default_factory=list, repr=False, init=False)
     car2_scores: List[int] = field(default_factory=list, repr=False, init=False)
+    state: State = State.BOTH_WAIT
+    debug: bool = False
 
-    def simulate_turn(self) -> None:
-        pass
+    def take_actions(self) -> None:
+        """Lets each car that hasn't crossed bridge take an action."""
+
+        if not self.car1.drove_over:
+            self.car1.take_action()
+        if not self.car2.drove_over:
+            self.car2.take_action()
 
     def check_state(self) -> None:
-        if self.car1.action == self.car2.action == Action.DRIVE:
+        """Checks game state after each car took action."""
+
+        c1 = self.car1
+        c2 = self.car2
+
+        if c1.action == c2.action == Action.DRIVE:
             self.state = State.CRASH
-        elif self.car1.action == self.car2.action == Action.WAIT:
+        elif c1.action == c2.action == Action.WAIT:
             self.state = State.BOTH_WAIT
-        elif self.car1.action == Action.DRIVE:
+        elif c1.action == Action.DRIVE:
             self.state = State.CAR_1_DROVE
-            self.car1.drove_over = True
-            self.car1.action = Action.WAIT
-            self.car1.score += 100
+            c1.drove_over = True
         else:
             self.state = State.CAR_2_DROVE
-            self.car2.drove_over = True
-            self.car2.action = Action.WAIT
-            self.car2.score += 100
+            c2.drove_over = True
 
     def give_rewards(self) -> None:
-        pass
+        """Gives rewards to each car based on current turn state.
+
+        First it finds reward value based on turn state, then calls
+        reward_action in each car as long as it hasn't driven over the bridge.
+        """
+
+        s = self.state
+        r1 = -200 if s is State.CRASH else 100 if s is State.CAR_1_DROVE else -10
+        r2 = -200 if s is State.CRASH else 100 if s is State.CAR_2_DROVE else -10
+
+        if not self.car1.drove_over or s is State.CAR_1_DROVE:
+            self.car1.reward_action(r1)
+        if not self.car2.drove_over or s is State.CAR_2_DROVE:
+            self.car2.reward_action(r2)
 
     def print_results(self) -> None:
+        """Prints out state and scores for cars for this round to console."""
+
         print(f"State: {str(self.state)} Scores: {self.car1.score} & {self.car2.score}")
 
-    def cleanup_game(self):
+    def cleanup_game(self) -> None:
+        """Adds scores for cars to list, resets state and drove_over for cars."""
+
         self.car1_scores.append(self.car1.score)
         self.car2_scores.append(self.car2.score)
         self.state = State.BOTH_WAIT
         self.car1.drove_over = self.car2.drove_over = False
 
-    def print_graph(self) -> None:
-        car1, = pyplot.plot(self.car1_scores, label="Car 1 scores")
-        car2, = pyplot.plot(self.car2_scores, label="Car 2 scores")
+    def show_graph(self) -> None:
+        """Shows graph with scores for each car after simulation."""
+
+        car1, = pyplot.plot(self.car1_scores, label="Car 1")
+        car2, = pyplot.plot(self.car2_scores, label="Car 2")
         pyplot.legend(handles=[car1, car2])
 
         pyplot.ylabel("Score")
+        pyplot.xlabel("Games")
 
         pyplot.show()
 
     def simulate_games(self, no_games: int) -> None:
+        """Simulates a given amount of games for the cars.
+
+        This method runs the whole simulation calling methods in class to play
+        out games and turns in the game. For each game it lets cars take action,
+        checks turn state, and gives rewards. After each game it cleans up and
+        runs again until it has ran the required amount of games. In the end it
+        shows plot of car scores.
+
+        Arguments:
+            no_games (int): Number of games to simulate.
+        """
+
         for _ in range(no_games):
-            print("New game")
-            while (not self.car1.drove_over or not self.car2.drove_over) and self.state != State.CRASH:
-                self.simulate_turn()
+            if self.debug:
+                print("New game")
+            while not (self.car1.drove_over and self.car2.drove_over) and self.state != State.CRASH:
+                self.take_actions()
                 self.check_state()
                 self.give_rewards()
-
-                self.print_results()
+                if self.debug:
+                    self.print_results()
 
             self.cleanup_game()
 
-        self.print_graph()
+        self.show_graph()
 
 
-@dataclass
-class RandomSimulation(Simulation):
-    def __init__(self):
-        super().__init__(RandomCar(), RandomCar())
+print("Zero Intelligence cars")
+random = Simulation(RandomCar(), RandomCar())
+random.simulate_games(1000)
 
-    def simulate_turn(self):
-        if not self.car1.drove_over:
-            self.car1.make_choice()
-        if not self.car2.drove_over:
-            self.car2.make_choice()
-
-    def give_rewards(self):
-        if self.state == State.CRASH:
-            r1 = r2 = -200
-        elif self.state == State.BOTH_WAIT:
-            r1 = r2 = -10
-        elif self.state == State.CAR_1_DROVE:
-            r1 = 100
-            r2 = -10
-        else:
-            r1 = -10
-            r2 = 100
-
-        if not self.car1.drove_over:
-            self.car1.score += r1
-        if not self.car2.drove_over:
-            self.car2.score += r2
-
-
-r = RandomSimulation()
-r.simulate_games(1000)
+print("Q-Learning cars")
+learn = Simulation(LearningCar(), LearningCar())
+learn.simulate_games(1000)
